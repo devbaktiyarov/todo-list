8067d60d6c5464b5b0ebe9f7ecc3e4959190200c
devbaktiyarov <devbaktiyarov@gmail.com>
2022-04-10 00:51:46 +0600

    created the structure and ui for the application

---
+++ src/components/todo-list-item.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './todo-list-item.css';
+
+const TodoListItem = ({ label, important = false }) => {
+
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  };
+
+  return (
+    <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={style}>
+        {label}
+      </span>
+      <span>
+        <button type="button"
+                className="btn btn-outline-success btn-sm ">
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm ">
+          <i className="fa fa-trash-o" />
+      </button>
+      </span>
+    </span>
+  );
+};
+
+export default TodoListItem;
